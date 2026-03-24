0cd4de70aa6558da6cce50eb2b9cade93faf652b
Vivo <ilya.tsygankov.191@mail.ru>
2026-03-24 10:30:22 +0300

    Initial commit

---
+++ src/main/java/com/ilya/src/controller/HelloController.java
@@ -0,0 +1,7 @@
+package com.ilya.src.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class HelloController {
+}
